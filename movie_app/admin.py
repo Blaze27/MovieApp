23757862a094196578e5a0273b7f6c49e079c084
Rahul Sharma <bbblaze27@gmail.com>
2021-04-25 02:54:23 +0530

    Register models in admin site

--- movie_app/admin.py
+++ movie_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Movie, Director, Genre, Studio
 
 # Register your models here.
+
+all_model = [
+    Movie,
+    Director,
+    Genre,
+    Studio,
+]
+
+for _ in all_model:
+    admin.site.register(_)
