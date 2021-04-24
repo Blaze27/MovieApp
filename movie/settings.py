a545290681d6e861eecef62b0878e6d5b682fc58
Rahul Sharma <bbblaze27@gmail.com>
2021-04-24 23:27:11 +0530

    Include app in installed_apps

--- movie/settings.py
+++ movie/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -118,3 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+
+# Changes made by developer in Project settings 
+
+INSTALLED_APPS += [
+    'movie_app',
+]
